5f9adb9cf3a6f3e0e8a785f7b891823616975472
Ender <woodwind.turbeville@gmail.com>
2020-10-30 00:01:34 -0500

    Added new animation at window open

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,3 +1,42 @@
 // Parralax Scrolling Animation
 var rellax = new Rellax('.rellax');
 
+// Anime Animations
+anime.timeline()
+  .add({
+    targets: '.fnt-1',
+    translateY: [100,0],
+    translateZ: 0,
+    opacity: [0,1],
+    scale: [1.5, 1],
+    rotateZ: [360, 330],
+    easing: "easeOutExpo",
+    duration: 1400,
+    delay: (el, i) => 300 + 30 * i
+});
+
+anime.timeline()
+  .add({
+    targets: '.fnt-2',
+    translateY: [100,0],
+    translateZ: 0,
+    opacity: [0,1],
+    scale: [1.5, 1],
+    rotateZ: [-180],
+    easing: "easeOutExpo",
+    duration: 1400,
+    delay: (el, i) => 400 + 30 * i
+});
+
+anime.timeline()
+  .add({
+    targets: '.fnt-3',
+    translateY: [100,0],
+    translateZ: 0,
+    opacity: [0,1],
+    rotateZ: [-360],
+    scale: [1.5, 1],
+    easing: "easeOutExpo",
+    duration: 1400,
+    delay: (el, i) => 500 + 30 * i
+});
